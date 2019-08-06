b037bf213d7dd78ce8af49d2dc9cd13cf8f44fad
Alex Delgado <delgadoalex@icloud.com>
2019-08-05 21:05:24 -0500

    implemented firebase utils, included ability to store autehnticated users into our firestore database

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'; 
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils'; 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'; 
 
 class App extends React.Component {
   constructor() {
@@ -17,19 +17,32 @@ class App extends React.Component {
     }
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
+  // Lifecycle method handling our user authentication
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth); 
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id, 
+              ...snapShot.data() 
+            }
+          })
+        }) 
+      } else {
+        this.setState({ currentUser: userAuth }); 
+      }
     })
-  }
+  };
 
   componentWillUnmount() {
     this.unsubscribeFromAuth(); 
   }
 
+  // Returns the indivdidual pages to our application. 
   render() {
     return (
       <div>
--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -2,6 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore'; 
 import 'firebase/auth'; 
 
+
+// Firebase config. 
 const config = {
     apiKey: "AIzaSyA6wjm3zaTPeBxoR96wDiwHlVrQCp_jIq4",
     authDomain: "crwn-db-912c2.firebaseapp.com",
@@ -12,14 +14,43 @@ const config = {
     appId: "1:921689109412:web:e838a9b53df60185"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return; 
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`); 
+
+    const snapShot = await userRef.get(); 
+
+    // checking to see if the 'snapShot' of our user data exists. 
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth; 
+        const createdAt = new Date(); 
+
+        try {
+            await userRef.set({
+                displayName, 
+                email, 
+                createdAt, 
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef; 
+    
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore(); 
 
+// Setting up authentication with Google. 
 const provider = new firebase.auth.GoogleAuthProvider(); 
 provider.setCustomParameters({ prompt: 'select_account'}); 
-export const signInWithGoogle = () => auth.signInWithPopUp(provider); 
+export const signInWithGoogle = () => auth.signInWithPopup(provider); 
 
 export default firebase; 
 
