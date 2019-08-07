cfb0519b79a7c36b10542d3ad800ae505c92c7da
Alex Delgado <delgadoalex@icloud.com>
2019-08-07 10:24:46 -0500

    Wrote root-reducer

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,2 +1,8 @@
-// This will be the code that combines all of our 'states' together - This is the root-reducer
+import { combineReducers } from 'redux'; 
 
+import userReducer from './user/user.reducer'; 
+
+// This will be the code that combines all of our 'states' together - This is the root-reducer
+export default combineReducers({
+    user: userReducer
+});
