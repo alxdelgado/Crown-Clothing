e2b3e476f9bd8ce49460c707f481384c6c97259d
Alex Delgado <delgadoalex@icloud.com>
2019-08-13 14:48:43 -0500

    added documentation to cart.selectors

--- crwn-clothing/src/redux/cart/cart.selectors.js
+++ crwn-clothing/src/redux/cart/cart.selectors.js
@@ -1,27 +1,13 @@
-// import { createSelector } from 'reselect'; 
+// 2 types of selectors
+// Input Select - does not use createSelector 
+// Output Selector - does use input selector and createSelector to build themselves. 
 
-// // 2 types of selectors
-// // Input Select - does not use createSelector 
-// // Output Selector - does use input selector and createSelector to build themselves. 
+// Input Selector - it is a function that gets the whole state and returns a slice of it 1 layer deep.
 
-// // Input Selector - it is a function that gets the whole state and returns a slice of it 1 layer deep.
+// createSelector takes two arguements
+// first arguement is a collection(array) of input selectors
+// second arguement is a function that returns the value we want out of the selector
 
-// const selectCart = state => state.cart; 
-
-// export const selectCartItems = createSelector( // createSelector takes two arguements 
-//     [selectCart], // first arguement is a collection(array) of input selectors 
-//     (cart) => cart.cartItems // second arguement is a function that returns the value we want out of the selector. 
-// ); 
-
-// export const selectCartItemsCount = createSelector(
-//     [selectCartItems], 
-//     cartItems => 
-//         cartItems.reduce(
-//             (accumulatedQuantity, cartItem) => 
-//                 accumulatedQuantity + cartItem.qauntity,
-//                 0
-//         )
-// );
 
 import { createSelector } from 'reselect';
 
