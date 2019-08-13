c9bd462fefdd2a0761ab5e8c90aea7b053aa0475
Alex Delgado <delgadoalex@icloud.com>
2019-08-13 14:04:42 -0500

    fixed error in reducer regarding adding items to cart and displaying them inside the cart-dropdown component

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,10 +1,36 @@
-import CartActionTypes from './cart.types';
+// import CartActionTypes from './cart.types';
+// import { addItemToCart } from './cart.utils'; 
 
-import { addItemToCart } from './cart.utils'; 
+// const INITIAL_STATE = {
+//   hidden: true, 
+//   cartItems: [] 
+// };
+
+// const cartReducer = (state = INITIAL_STATE, action) => {
+//   switch (action.type) {
+//     case CartActionTypes.TOGGLE_CART_HIDDEN:
+//       return {
+//         ...state,
+//         hidden: !state.hidden
+//       };
+//     case CartActionTypes.ADD_ITEM:  
+//       return {
+//         ...state, 
+//         cartItmes: addItemToCart(state.cartItems, action.payload) 
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default cartReducer;
+
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true, 
-  cartItems: [] // add the new value 'cartItems' into the inital state of our reducer 
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -14,11 +40,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
-    case CartActionTypes.ADD_ITEM: // new case listening for our addItem action 
+    case CartActionTypes.ADD_ITEM:
       return {
-        ...state, 
-        cartItmes: addItemToCart(state.cartItems, action.payload) 
-      }
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
     default:
       return state;
   }
