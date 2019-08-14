0bc2d3a419c2245c91dd935c0f2be4d6293c279b
Alex Delgado <delgadoalex@icloud.com>
2019-08-13 20:34:43 -0500

    added a remove item from cart feature. modified redux cart and checkout-item component

--- crwn-clothing/src/redux/cart/cart.types.js
+++ crwn-clothing/src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN', 
-    ADD_ITEM: 'ADD_ITEM' // make an new action that is going to modify our new prooperty which is 'cartItems' array 
+    ADD_ITEM: 'ADD_ITEM', // make an new action that is going to modify our new prooperty which is 'cartItems' array 
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 }
 
 export default CartActionTypes;
