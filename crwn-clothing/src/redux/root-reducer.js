366821610d700a0692aa7eec1f7e05625535ef5a
Alex Delgado <delgadoalex@icloud.com>
2019-08-20 13:30:14 -0500

    moved our Collection (items) state into redux - new redux files in folder shop

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -6,6 +6,7 @@ import storage from 'redux-persist/lib/storage'; // we will get the actual local
 import userReducer from './user/user.reducer'; // always add your reducers to the 'root-reducer'.
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer'; 
+import shopReducer from './shop/shop.reducer'; 
 
 // This is the JSON object that respresents the possible configurations we want redux persist to use 
 const persistConfig = {
@@ -14,13 +15,13 @@ const persistConfig = {
     whitelist: ['cart'] // this lets redux persist know that the only object we want to 'whitelist' (persist) is the cart
 }
 
+// This will be the code that combines all of our 'states' together - This is the root-reducer
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    directory: directoryReducer
+    directory: directoryReducer,
+    shop: shopReducer
 
 });
 
-
-// This will be the code that combines all of our 'states' together - This is the root-reducer
 export default persistReducer(persistConfig, rootReducer); 
