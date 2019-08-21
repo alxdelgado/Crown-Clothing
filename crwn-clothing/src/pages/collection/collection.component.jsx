5062800a8547ea98f72657c616639be9a1e6dc56
Alex Delgado <delgadoalex@icloud.com>
2019-08-21 11:15:24 -0500

    modified collection page component w/ dynamic routing and redux

--- crwn-clothing/src/pages/collection/collection.component.jsx
+++ crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,15 +1,22 @@
 import React from 'react'; 
+import { connect } from 'react-redux'; 
 
-import CollectionItem from '../../components/collection-item/collection-item.component'; 
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';  
 
 import './collection.styles.scss'; 
 
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
     <div className='collection-page'>
         <h2>COLLECTION PAGE</h2>
     </div>
 ); 
 
-export default CollectionPage; 
+// 'ownProps' - property of the component we are wrapping in our connect()
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage); 
 
--- crwn-clothing/src/redux/shop/shop.selectors.js
+++ crwn-clothing/src/redux/shop/shop.selectors.js
@@ -1,8 +1,26 @@
 import { createSelector } from 'reselect'; 
 
+// this object will match the string value from our URL param to the respective ID 
+// The reason we are using an object is because our URL param is a string and the ID we want to match us a number
+const COLLECTION_ID_MAP = {
+    hats: 1, 
+    sneakers: 2,
+    jackets: 3, 
+    womens: 4, 
+    mens: 5
+}
+
 const selectShop = state => state.shop; 
 
 export const selectCollections = createSelector(
     [selectShop], 
     shop => shop.collections
 );
+
+// selector - here our collection.id is matching the url parameter of our COLLECTION_ID_MAP
+// 'createSelector' (curryFunction) - a function that takes the state and runs it through the selector flow we have written below
+export const selectCollection = collectionUrlParam => 
+    createSelector(
+        [selectCollections], 
+        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    )
