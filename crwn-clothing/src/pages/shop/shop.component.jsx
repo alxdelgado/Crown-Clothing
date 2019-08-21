72ea496f4f58cf81ed64c61c19ad7225e0dcbf94
Alex Delgado <delgadoalex@icloud.com>
2019-08-21 10:56:04 -0500

    added nested routing to our ShopPage component

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom'; 
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'; 
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> 
     </div>
 );
 
 export default ShopPage; 
+
+// Notes:
+    // Nested Routing - we want our collection to dynamically pluck off the right category from our reducer, so that it knows which items to display related to the category we are on. 
+    // "match.path" - allows us to access 'categoryId' as a parameter on the match object when we are inside of our category page
