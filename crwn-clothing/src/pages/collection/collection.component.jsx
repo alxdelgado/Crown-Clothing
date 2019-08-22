466fe8da940783d2f387d79e597fd32881b7be5b
Alex Delgado <delgadoalex@icloud.com>
2019-08-22 11:14:09 -0500

    updated our SHOP_DATA w/ data normilzation - moving it from an array to objects based on the collection

--- crwn-clothing/src/pages/collection/collection.component.jsx
+++ crwn-clothing/src/pages/collection/collection.component.jsx
@@ -7,11 +7,19 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss'; 
 
 
-const CollectionPage = ({ collection }) => (
-    <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
-    </div>
-); 
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    );
+};  
 
 // 'ownProps' - property of the component we are wrapping in our connect()
 const mapStateToProps = (state, ownProps) => ({
