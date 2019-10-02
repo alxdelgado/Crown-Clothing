8ea6ee562acf7a0a0ac0f68e981c7fdd50aec876
Alex Delgado <delgadoalex@icloud.com>
2019-10-01 19:37:12 -0500

    Created a new shop selector to convert our Collections Object into an array

--- crwn-clothing/src/redux/shop/shop.selectors.js
+++ crwn-clothing/src/redux/shop/shop.selectors.js
@@ -7,6 +7,12 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections], 
+    collections => Object.keys(collections).map(key => collections[key]) // we want to get all the keys and map over the keys to get out the value of the collections object - which will give us an array of our items. 
+); 
+
 // selector - here our collections is matching the collections url parameter
 // 'createSelector' (curryFunction) - a function that takes the state and runs it through the selector flow we have written below
 export const selectCollection = collectionUrlParam => 
