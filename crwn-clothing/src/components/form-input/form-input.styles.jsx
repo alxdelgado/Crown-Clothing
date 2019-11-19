18f9b230347ff34607d69021c038f7e4969b2d50
Alex Delgado <delgadoalex@icloud.com>
2019-11-19 17:39:16 -0600

    fixed error with form-input-styles css block

--- crwn-clothing/src/components/form-input/form-input.styles.jsx
+++ crwn-clothing/src/components/form-input/form-input.styles.jsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components'; 
+import styled, { css } from 'styled-components'; 
 
 const subColor = 'grey'; 
 const mainColor = 'black'; 
 
-const shrinkLabelStyles = styled.css`
+const shrinkLabelStyles = css`
     top: -14px;
     font-size: 12px;
     color: ${mainColor};
