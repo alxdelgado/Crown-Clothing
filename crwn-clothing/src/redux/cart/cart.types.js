28ecb6a9976c1a3c5aba4d33c99f84e3c875b8e4
Alex Delgado <delgadoalex@icloud.com>
2019-12-03 15:16:22 -0600

    refactored our sign-in component to use react hooks

--- crwn-clothing/src/redux/cart/cart.types.js
+++ crwn-clothing/src/redux/cart/cart.types.js
@@ -1,6 +1,6 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN', 
-    ADD_ITEM: 'ADD_ITEM', // make an new action that is going to modify our new prooperty which is 'cartItems' array 
+    ADD_ITEM: 'ADD_ITEM', // make an new action that is going to modify our new property which is 'cartItems' array 
     REMOVE_ITEM: 'REMOVE_ITEM',
     CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 }
